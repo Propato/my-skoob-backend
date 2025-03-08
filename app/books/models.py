0e8a6a217ef72dcf851a33398e2779e8f8c7d957
Propato <david123propato@gmail.com>
2025-03-08 00:24:48 -0300

    db init

--- app/books/models.py
+++ app/books/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
+
+from users.models import Users
 
 # Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    pages = models.IntegerField(blank=True)
+    release = models.DateField(blank=True)
+    validate = models.BooleanField(default=False)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['name', 'author'], name='unique_book', violation_error_message="This book has already been registered")
+        ]
+
+    def __str__(self):
+        return f'{self.author}:{self.name}:{self.release} - {self.validate}'
+
+class Review(models.Model):
+    userId = models.ForeignKey(Users, on_delete=models.CASCADE)
+    bookId = models.ForeignKey(Book, on_delete=models.CASCADE)
+    
+    stars = models.IntegerField(blank=True, validators=[MaxValueValidator(100)])
+    comment = models.CharField(max_length=300, blank=True)
+
+    STATUS = [
+        (0, "Read"),
+        (1, "Reading"),
+        (2, "Drop"),
+        (3, "List"),
+    ]
+    status = models.IntegerField(choices=STATUS)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['userId', 'bookId'], name='unique_user_review_per_book', violation_error_message="User has already made a review for this book")
+        ]
+
+    def __str__(self):
+        return f'[{self.userId}]:[{self.bookId}]\n\t{self.get_status_display()} - {self.stars} - {self.comment}'
--- app/users/models.py
+++ app/users/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+from datetime import date
 
 # Create your models here.
+
+class Users(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=60)
+    password = models.CharField(max_length=200)
+
+    GENDERS = [
+        ("F", "Female"),
+        ("M", "Male"),
+        ("O", "Others"),
+    ]
+    gender = models.CharField(max_length=1, choices=GENDERS, blank=True)
+    
+    def validate_birthday(value):
+        today = date.today()
+        min_age_date = date(today.year - 12, today.month, today.day)
+        if value > min_age_date:
+            raise ValidationError(
+                _('You must be at least 12 years old.'),
+                params={'value': value},
+            )
+    birthday = models.DateField(blank=True, validators=[validate_birthday])
+
+    @property
+    def years(self):
+        today = date.today()
+        return today.year - self.birthday.year - ((today.month, today.day) < (self.birthday.month, self.birthday.day))
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['email'], name='unique_email', violation_error_message="This email has already been registered")
+        ]
+
+    def __str__(self):
+        return f'{self.name}, {self.years}'
